fff0d52bb618b8834e25233f539ac9b1628b444b
Ferdiansyah <ferdif9996@gmail.com>
2023-11-10 12:24:29 +0700

    add alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "url";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
+      { find: '@stores', replacement: fileURLToPath(new URL('./src/stores', import.meta.url)) },
+      { find: '@styles', replacement: fileURLToPath(new URL('./src/styles', import.meta.url)) },
+    ],
+  },
 })
